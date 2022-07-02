d26d7fb9280d6e5da95e92a02db9a528f1af6017
imp5imp5 <34108613+imp5imp5@users.noreply.github.com>
2022-07-02 11:49:05 +0300

    Update m.cpp

--- m.cpp
+++ m.cpp
@@ -2,7 +2,7 @@
 #include "vecMath_test.h"
 int main()
 {
-//  mathtest::base_test();
+  mathtest::base_test();
 //  mathtest::advanced_test();
   mathtest::profile(20000005);
 //  mathtest::profile(1);
